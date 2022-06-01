dd33a856d6b5d5a5a5887b03030b5bf400e636ce
devevandro <dev.evandro@gmail.com>
2022-05-31 21:12:25 -0300

    refactor: make encrypter

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,35 +1,40 @@
-import { Encrypter } from '../../protocols/encrypter';
-import { DbAddAccount } from './db-add-account';
+import { Encrypter } from "../../protocols/encrypter";
+import { DbAddAccount } from "./db-add-account";
 
 interface SutTypes {
-  sut: DbAddAccount,
-  encrypterStub: Encrypter
+  sut: DbAddAccount;
+  encrypterStub: Encrypter;
 }
 
-const makeSut = (): SutTypes => {
-    class EncrypterStub {
-    async encrypt (value: string): Promise<string> {
-      return new Promise(resolve => resolve('hashed_password'));
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt(value: string): Promise<string> {
+      return new Promise((resolve) => resolve("hashed_password"));
     }
-  };
-  const encrypterStub = new EncrypterStub();
+  }
+
+  return new EncrypterStub();
+};
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypter();
   const sut = new DbAddAccount(encrypterStub);
   return {
     sut,
-    encrypterStub
-  }
-}
+    encrypterStub,
+  };
+};
 
-describe('DbAddAccount Usecase', () => {
-  test('Should call Encrypter with correct password', async () => {
+describe("DbAddAccount Usecase", () => {
+  test("Should call Encrypter with correct password", async () => {
     const { sut, encrypterStub } = makeSut();
-    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
+    const encryptSpy = jest.spyOn(encrypterStub, "encrypt");
     const accountData = {
-      name: 'valid_name',
-      email: 'valid_email',
-      password: 'valid_password',
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password",
     };
     await sut.add(accountData);
-    expect(encryptSpy).toHaveBeenCalledWith('valid_password');
-  })
+    expect(encryptSpy).toHaveBeenCalledWith("valid_password");
+  });
 });
